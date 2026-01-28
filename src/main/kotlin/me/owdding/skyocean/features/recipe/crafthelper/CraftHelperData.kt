0dddcf8f17b9c46333604aee459318af8d798951
JankleDev <91403810+JankleDev@users.noreply.github.com>
2026-01-29 04:30:47 +0530

    more deletes

--- src/main/kotlin/me/owdding/skyocean/features/recipe/crafthelper/CraftHelperData.kt
+++
@@ -1,8 +0,0 @@
-package me.owdding.skyocean.features.recipe.crafthelper
-
-import me.owdding.skyocean.features.recipe.ItemLikeIngredient
-import me.owdding.skyocean.features.recipe.crafthelper.data.CraftHelperRecipeType
-
-abstract class CraftHelperRecipe(val type: CraftHelperRecipeType, val canModifyCount: Boolean) {
-    abstract fun resolve(resetLayout: () -> Unit, clear: () -> Unit): Pair<ContextAwareRecipeTree, ItemLikeIngredient>?
-}
